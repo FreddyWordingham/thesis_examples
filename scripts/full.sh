e9968849a038b0531d72df6a29ce8a1a75a1c7c9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-03-14 18:03:52 +0000

    Added master central grid to cartography examples.

---
+++ scripts/full.sh
@@ -0,0 +1,3 @@
+cartographer input output/cartographer/skin cartographer/skin.json5;
+cartographer input output/cartographer/tumour cartographer/tumour.json5;
+cartographer input output/cartographer/thick_tumour cartographer/thick_tumour.json5;
