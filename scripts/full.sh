ac33905eb0e209616bfc3fea38ccfbb89ade0c99
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-03-29 00:39:53 +0100

    Added pdt mcrt parameterisation.

--- scripts/full.sh
+++ scripts/full.sh
@@ -5,7 +5,7 @@ mv output/oxy_diff.nc input/res/maps/;
 babbage input/ output/ babbage/build_oxy_source_map.json5;
 mv output/oxy_source.nc input/res/maps/;
 diffuse input/ output/diffuse/oxy diffuse/oxy.json5;
-mv output/diffuse/oxy/099_diff.nc input/res/maps/oxy_init.nc;
+mv output/diffuse/oxy/009_diff.nc input/res/maps/oxy_init.nc;
 
 #   PpIX setup.
 cartographer input output/cartographer/cream cartographer/cream.json5;
@@ -20,10 +20,11 @@ mv output/multipliers.nc input/res/maps/;
 mv output/cartographer/tumour/map_\{tumour\}.nc input/res/maps/tumour.nc
 reactor input/ output/reactor/ppix reactor/ppix.json5;
 mv output/reactor/ppix/099_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
+mv output/reactor/ppix/099_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
 
 #   PDT phase.
-mcrt input/ output/mcrt/tumour mcrt/tumour.json5;
-mv output/mcrt/tumour/shift_map.nc input/res/maps/udens.nc;
+mcrt input/ output/mcrt/deep_tumour mcrt/deep_tumour.json5;
+mv output/mcrt/deep_tumour/shift_density.nc input/res/maps/udens.nc;
 reactor input/ output/reactor/pdt reactor/pdt.json5;
 babbage input/ output/ babbage/build_kill_map.json5;
 # -- Repeate for an addition 10min --
