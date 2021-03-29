a8833168728f583ed1384a4fadd23f8a3d178fb9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-03-29 01:16:18 +0100

    Final parameterisation.

--- scripts/full.sh
+++ scripts/full.sh
@@ -13,21 +13,15 @@ babbage input/ output/ babbage/build_ala_init_map.json5;
 mv output/ala_init.nc input/res/maps/;
 babbage input/ output/ babbage/build_ala_diff_map.json5;
 mv output/ala_diff.nc input/res/maps/;
-
 cartographer input output/cartographer/deep_tumour cartographer/deep_tumour.json5;
 babbage input/ output/ babbage/build_multipliers_map.json5;
 mv output/multipliers.nc input/res/maps/;
-mv output/cartographer/tumour/map_\{tumour\}.nc input/res/maps/tumour.nc
 reactor input/ output/reactor/ppix reactor/ppix.json5;
-mv output/reactor/ppix/099_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
-mv output/reactor/ppix/099_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
+mv output/reactor/ppix/009_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
+mv output/reactor/ppix/009_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
 
 #   PDT phase.
 mcrt input/ output/mcrt/deep_tumour mcrt/deep_tumour.json5;
 mv output/mcrt/deep_tumour/shift_density.nc input/res/maps/udens.nc;
 reactor input/ output/reactor/pdt reactor/pdt.json5;
 babbage input/ output/ babbage/build_kill_map.json5;
-# -- Repeate for an addition 10min --
-
-
-# cartographer input output/cartographer/thick_tumour cartographer/thick_tumour.json5;
