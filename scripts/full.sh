b78187a65289268f37782868934b51660b9501bb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-03-30 13:34:26 +0100

    Made shallow tumour parameterisation more explicit.

--- scripts/full.sh
+++ scripts/full.sh
@@ -1,27 +1,27 @@
 #   Oxygen setup.
 cartographer input output/cartographer/skin cartographer/skin.json5;
 babbage input/ output/ babbage/build_oxy_diff_map.json5;
-mv output/oxy_diff.nc input/res/maps/;
+cp output/oxy_diff.nc input/res/maps/;
 babbage input/ output/ babbage/build_oxy_source_map.json5;
-mv output/oxy_source.nc input/res/maps/;
+cp output/oxy_source.nc input/res/maps/;
 diffuse input/ output/diffuse/oxy diffuse/oxy.json5;
-mv output/diffuse/oxy/008_diff.nc input/res/maps/oxy_init.nc;
+cp output/diffuse/oxy/008_diff.nc input/res/maps/oxy_init.nc;
 
 #   PpIX setup.
 cartographer input output/cartographer/cream cartographer/cream.json5;
 babbage input/ output/ babbage/build_ala_init_map.json5;
-mv output/ala_init.nc input/res/maps/;
+cp output/ala_init.nc input/res/maps/;
 babbage input/ output/ babbage/build_ala_diff_map.json5;
-mv output/ala_diff.nc input/res/maps/;
+cp output/ala_diff.nc input/res/maps/;
 cartographer input output/cartographer/deep_tumour cartographer/deep_tumour.json5;
 babbage input/ output/ babbage/build_deep_tumour_multipliers_map.json5;
-mv output/multipliers.nc input/res/maps/;
+cp output/multipliers.nc input/res/maps/;
 reactor input/ output/reactor/ppix reactor/ppix.json5;
-mv output/reactor/ppix/008_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
-mv output/reactor/ppix/008_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
+cp output/reactor/ppix/008_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
+cp output/reactor/ppix/008_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
 
 #   PDT phase.
 mcrt input/ output/mcrt/deep_tumour mcrt/deep_tumour.json5;
-mv output/mcrt/deep_tumour/shift_density.nc input/res/maps/udens.nc;
+cp output/mcrt/deep_tumour/shift_density.nc input/res/maps/udens.nc;
 reactor input/ output/reactor/pdt reactor/pdt.json5;
 babbage input/ output/ babbage/build_kill_map.json5;
