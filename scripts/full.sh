b8afcbffb95d5aff423a246be73e0aa05c44c5de
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-04-03 22:15:10 +0100

    Improved naming scheme.

--- scripts/full.sh
+++ scripts/full.sh
@@ -15,17 +15,17 @@ babbage input/ output/ babbage/build_map_diff_ala.json5;
 cp output/diff_ala.nc input/res/maps/;
 
 
-# #   Shallow tumour
-# #   PpIX setup.
-# cartographer input output/cartographer/shallow_tumour cartographer/shallow_tumour.json5;
-# babbage input/ output/ babbage/build_shallow_tumour_multipliers_map.json5;
-# cp output/multipliers.nc input/res/maps/shallow_multipliers.nc;
-# reactor input/ output/reactor/ppix/shallow reactor/ppix_shallow.json5;
-# cp output/reactor/ppix/shallow/008_\{ppix\}_diff.nc input/res/maps/ppix_init_shallow.nc
-# cp output/reactor/ppix/shallow/008_\{ala\}_diff.nc input/res/maps/ala_init_shallow.nc
+#   Shallow tumour
+#   PpIX setup.
+cartographer input output/cartographer/shallow_tumour cartographer/shallow_tumour.json5;
+babbage input/ output/ babbage/build_map_multipliers_tumour_shallow.json5;
+cp output/multipliers_shallow.nc input/res/maps/;
+reactor input/ output/reactor/ppix/shallow reactor/ppix_shallow.json5;
+cp output/reactor/ppix/shallow/008_\{ppix\}_diff.nc input/res/maps/init_ppix_shallow.nc
+cp output/reactor/ppix/shallow/008_\{ala\}_diff.nc input/res/maps/init_ala_shallow.nc
 
-# # #   PDT phase.
-# mcrt input/ output/mcrt/shallow mcrt/shallow_tumour.json5;
-# cp output/mcrt/shallow/shift_density.nc input/res/maps/shallow_udens.nc;
-# reactor input/ output/reactor/pdt/shallow reactor/pdt_shallow.json5;
-# babbage input/ output/ babbage/build_kill_map.json5;
+# #   PDT phase.
+mcrt input/ output/mcrt/shallow mcrt/tumour_shallow.json5;
+cp output/mcrt/shallow/shift_density.nc input/res/maps/udens_shallow.nc;
+reactor input/ output/reactor/pdt/shallow reactor/pdt_shallow.json5;
+babbage input/ output/ babbage/build_kill_map.json5;
