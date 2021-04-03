9ad18cfa3f341bac0be75c5e19ce9608d1d7d896
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-04-03 20:58:43 +0100

    Model differentiation.

--- scripts/full.sh
+++ scripts/full.sh
@@ -1,70 +1,48 @@
-#   Oxygen setup.
-cartographer input output/cartographer/skin cartographer/skin.json5;
-babbage input/ output/ babbage/build_oxy_diff_map.json5;
-cp output/oxy_diff.nc input/res/maps/;
-babbage input/ output/ babbage/build_oxy_source_map.json5;
-cp output/oxy_source.nc input/res/maps/;
-diffuse input/ output/diffuse/oxy diffuse/oxy.json5;
-cp output/diffuse/oxy/008_diff.nc input/res/maps/oxy_init.nc;
+# #   Oxygen setup.
+# cartographer input output/cartographer/skin cartographer/skin.json5;
+# babbage input/ output/ babbage/build_oxy_diff_map.json5;
+# cp output/oxy_diff.nc input/res/maps/;
+# babbage input/ output/ babbage/build_oxy_source_map.json5;
+# cp output/oxy_source.nc input/res/maps/;
+# diffuse input/ output/diffuse/oxy diffuse/oxy.json5;
+# cp output/diffuse/oxy/008_diff.nc input/res/maps/oxy_init.nc;
+
+# #   ALA setup.
+# cartographer input output/cartographer/cream cartographer/cream.json5;
+# babbage input/ output/ babbage/build_ala_init_map.json5;
+# cp output/ala_init.nc input/res/maps/;
+# babbage input/ output/ babbage/build_ala_diff_map.json5;
+# cp output/ala_diff.nc input/res/maps/;
+
 
 # #   Shallow tumour
 # #   PpIX setup.
-# cartographer input output/cartographer/cream cartographer/cream.json5;
-# babbage input/ output/ babbage/build_ala_init_map.json5;
-# cp output/ala_init.nc input/res/maps/;
-# babbage input/ output/ babbage/build_ala_diff_map.json5;
-# cp output/ala_diff.nc input/res/maps/;
 # cartographer input output/cartographer/shallow_tumour cartographer/shallow_tumour.json5;
 # babbage input/ output/ babbage/build_shallow_tumour_multipliers_map.json5;
-# cp output/multipliers.nc input/res/maps/;
-# reactor input/ output/reactor/ppix reactor/ppix.json5;
-# cp output/reactor/ppix/008_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
-# cp output/reactor/ppix/008_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
+# cp output/multipliers.nc input/res/maps/shallow_multipliers.nc;
+# reactor input/ output/reactor/ppix/shallow reactor/ppix_shallow.json5;
+# cp output/reactor/ppix/shallow/008_\{ppix\}_diff.nc input/res/maps/ppix_init_shallow.nc
+# cp output/reactor/ppix/shallow/008_\{ala\}_diff.nc input/res/maps/ala_init_shallow.nc
 
-# #   PDT phase.
-# mcrt input/ output/mcrt mcrt/shallow_tumour.json5;
-# cp output/mcrt/shift_density.nc input/res/maps/udens.nc;
-# reactor input/ output/reactor/pdt reactor/pdt.json5;
+# # #   PDT phase.
+# mcrt input/ output/mcrt/shallow mcrt/shallow_tumour.json5;
+# cp output/mcrt/shallow/shift_density.nc input/res/maps/shallow_udens.nc;
+# reactor input/ output/reactor/pdt/shallow reactor/pdt_shallow.json5;
 # babbage input/ output/ babbage/build_kill_map.json5;
 
 
-# #   Thick tumour
-# #   PpIX setup.
-# cartographer input output/cartographer/cream cartographer/cream.json5;
-# babbage input/ output/ babbage/build_ala_init_map.json5;
-# cp output/ala_init.nc input/res/maps/;
-# babbage input/ output/ babbage/build_ala_diff_map.json5;
-# cp output/ala_diff.nc input/res/maps/;
-# cartographer input output/cartographer/thick_tumour cartographer/thick_tumour.json5;
-# babbage input/ output/ babbage/build_thick_tumour_multipliers_map.json5;
-# cp output/multipliers.nc input/res/maps/;
-# reactor input/ output/reactor/ppix reactor/ppix.json5;
-# cp output/reactor/ppix/008_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
-# cp output/reactor/ppix/008_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
+
+#   Thick tumour
+#   PpIX setup.
+cartographer input output/cartographer/thick_tumour cartographer/thick_tumour.json5;
+babbage input/ output/ babbage/build_thick_tumour_multipliers_map.json5;
+cp output/multipliers.nc input/res/maps/thick_multipliers.nc;
+reactor input/ output/reactor/ppix/thick reactor/ppix_thick.json5;
+cp output/reactor/ppix/thick/008_\{ppix\}_diff.nc input/res/maps/ppix_init_thick.nc
+cp output/reactor/ppix/thick/008_\{ala\}_diff.nc input/res/maps/ala_init_thick.nc
 
 # #   PDT phase.
-# mcrt input/ output/mcrt mcrt/thick_tumour.json5;
-# cp output/mcrt/shift_density.nc input/res/maps/udens.nc;
-# reactor input/ output/reactor/pdt reactor/pdt.json5;
-# babbage input/ output/ babbage/build_kill_map.json5;
-
-
-# #   Deep tumour
-# #   PpIX setup.
-# cartographer input output/cartographer/cream cartographer/cream.json5;
-# babbage input/ output/ babbage/build_ala_init_map.json5;
-# cp output/ala_init.nc input/res/maps/;
-# babbage input/ output/ babbage/build_ala_diff_map.json5;
-# cp output/ala_diff.nc input/res/maps/;
-# cartographer input output/cartographer/deep_tumour cartographer/deep_tumour.json5;
-# babbage input/ output/ babbage/build_deep_tumour_multipliers_map.json5;
-# cp output/multipliers.nc input/res/maps/;
-# reactor input/ output/reactor/ppix reactor/ppix.json5;
-# cp output/reactor/ppix/008_\{ppix\}_diff.nc input/res/maps/ppix_init.nc
-# cp output/reactor/ppix/008_\{ala\}_diff.nc input/res/maps/ala_init_2.nc
-
-# #   PDT phase.
-# mcrt input/ output/mcrt mcrt/deep_tumour.json5;
-# cp output/mcrt/shift_density.nc input/res/maps/udens.nc;
-# reactor input/ output/reactor/pdt reactor/pdt.json5;
-# babbage input/ output/ babbage/build_kill_map.json5;
+mcrt input/ output/mcrt/thick mcrt/thick_tumour.json5;
+cp output/mcrt/thick/shift_density.nc input/res/maps/thick_udens.nc;
+reactor input/ output/reactor/pdt/thick reactor/pdt_thick.json5;
+babbage input/ output/ babbage/build_kill_map.json5;
